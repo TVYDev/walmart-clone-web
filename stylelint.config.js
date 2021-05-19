0bc49b4f32c257ba67f4511f894e5ab81a1f84a6
Vannyou <vannyoutang23@gmail.com>
2021-05-19 11:13:32 +0700

    fix: :bug: fix tailwind not working after adding tailwind.css

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: ['layer'],
+        ignoreAtRules: ['apply', 'layer', 'tailwind'],
       },
     ],
   },
