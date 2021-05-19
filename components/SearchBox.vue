7bd9596797b954d33961a3e25aa6f01515b089d2
Vannyou <vannyoutang23@gmail.com>
2021-05-19 22:21:46 +0700

    chore: :rocket: update SearchBox component for md size

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="flex flex-row items-center rounded-full overflow-hidden">
+    <button class="hidden md:block bg-white text-black px-5 py-1">
+      <i class="fas fa-caret-down text-xs" />
+    </button>
+    <!-- TODO: Find solution to set button with utility class when input is focused -->
     <input
       type="text"
-      class="px-3 py-2 flex-grow w-full text-xs"
+      class="px-3 py-2 md:pl-0 md:pr-3 flex-grow w-full text-xs outline-none"
       placeholder="Search Walmart.com"
     />
-    <div class="bg-accent px-3 py-1">
+    <button class="bg-accent px-3 py-1">
       <i class="fas fa-search text-primary text-sm" />
-    </div>
+    </button>
   </div>
 </template>
