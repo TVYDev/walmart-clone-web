ec2b127c2176ca36a924c84f61280a22097aa939
Vannyou <vannyoutang23@gmail.com>
2021-05-20 12:07:58 +0700

    chore: :rocket: add Tab component

---
+++ components/common/Tabs.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="flex flex-row">
+    <li
+      v-for="tab in tabs"
+      :key="tab.text"
+      class="
+        cursor-pointer
+        flex-1
+        sm:flex-initial
+        sm:w-40
+        py-2.5
+        px-3
+        text-xs
+        font-semibold
+        text-center
+        border-b-2 border-transparent
+        whitespace-nowrap
+      "
+      :class="{
+        'border-accent': tab.active,
+        'hover:border-secondary': !tab.active,
+      }"
+    >
+      {{ tab.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    tabs: {
+      type: Array,
+      default: () => [],
+    },
+  },
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <header>
+      <CommonTabs :tabs="navTabs" class="bg-primary text-white" />
       <NavBar />
     </header>
     <main>
@@ -8,3 +9,23 @@
     </main>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      navTabs: [
+        {
+          text: 'Pickup & delivery',
+          active: false,
+        },
+        {
+          text: 'Walmart.com',
+          active: true,
+        },
+      ],
+    };
+  },
+});
+</script>
