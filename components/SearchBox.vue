122a8d221c6b8f76e4655ad65cf8f93120689233
Vannyou <vannyoutang23@gmail.com>
2021-05-20 16:46:00 +0700

    chore: :rocket: add DropdownCard component

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="flex flex-row items-center rounded-full overflow-hidden">
-    <button class="hidden md:block bg-white text-black px-5 py-1">
-      <i class="fas fa-caret-down text-xs" />
-    </button>
-    <!-- TODO: Find solution to set button with utility class when input is focused -->
-    <input
-      type="text"
-      class="px-3 py-2 md:pl-0 md:pr-3 flex-grow w-full text-xs outline-none"
-      placeholder="Search Walmart.com"
-    />
-    <button class="bg-accent px-3 py-1">
-      <i class="fas fa-search text-primary text-sm" />
-    </button>
-  </div>
+  <CommonDropdownCard>
+    <template #default="{ dropdownToggler }">
+      <div class="flex flex-row items-center rounded-full overflow-hidden">
+        <button
+          class="hidden lg:block bg-white text-black px-5 py-1"
+          @click="dropdownToggler"
+        >
+          <i class="fas fa-caret-down text-xs" />
+        </button>
+        <!-- TODO: Find solution to set button with utility class when input is focused -->
+        <input
+          type="text"
+          class="
+            px-3
+            py-2
+            lg:pl-0
+            md:pr-3
+            flex-grow
+            w-full
+            text-xs
+            outline-none
+          "
+          placeholder="Search Walmart.com"
+        />
+        <button class="bg-accent px-3 py-1">
+          <i class="fas fa-search text-primary text-sm" />
+        </button>
+      </div>
+    </template>
+    <template #content>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, vel!
+    </template>
+  </CommonDropdownCard>
 </template>
---
+++ components/common/DropdownCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="relative">
+    <slot :dropdownToggler="dropdownToggler" />
+    <transition name="slide-vertical">
+      <div
+        v-if="isShown"
+        class="
+          w-full
+          hidden
+          lg:block
+          absolute
+          top-9
+          bg-white
+          rounded-2xl
+          p-3
+          shadow-lg
+        "
+      >
+        <slot name="content" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  data() {
+    return {
+      isShown: false,
+    };
+  },
+  methods: {
+    dropdownToggler() {
+      this.isShown = !this.isShown;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.slide-vertical-enter-active,
+.slide-vertical-leave-active {
+  transition: all 0.2s;
+}
+.slide-vertical-enter,
+.slide-vertical-leave-to {
+  transform: translateY(-5px);
+  opacity: 0;
+}
+</style>
