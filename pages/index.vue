7ed93bede3e1d5d20b9be576b5b13aba765af140
Vannyou <vannyoutang23@gmail.com>
2021-05-23 15:34:33 +0700

    chore: :rocket: add banner walmart plus on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <Logo />
-    <Logo />
+    <CommonBanner>
+      <div class="flex flex-row items-center justify-center">
+        <img
+          src="~/assets/images/walmart-plus.svg"
+          alt=""
+          class="h-7 md:h-11 mr-3"
+        />
+        <div class="text-xs md:text-sm text-white">
+          <div class="md:hidden mb-2">
+            W+ no order minimum shipping. <br />
+            Restrictions apply.
+          </div>
+          <div class="hidden md:block">
+            Free shipping with no order minimum required. Restrictions apply.
+          </div>
+          <NuxtLink to="#" class="underline">Try it free</NuxtLink>
+        </div>
+      </div>
+    </CommonBanner>
     <Logo />
     <Logo />
     <Logo />
@@ -9,7 +26,3 @@
     <Logo />
   </div>
 </template>
-
-<script>
-export default {};
-</script>
