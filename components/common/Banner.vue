2bd8f00e8eead497b010f2792e2d1bbb2b973b53
Vannyou <vannyoutang23@gmail.com>
2021-05-23 15:33:44 +0700

    chore: :rocket: add Banner component

---
+++ components/common/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+  <transition name="fade">
+    <div
+      v-if="isShown"
+      class="flex flex-row items-center justify-between px-2 py-2 bg-secondary"
+    >
+      <div class="flex-1"><slot /></div>
+      <i
+        v-if="closable"
+        class="fas fa-times ml-3 text-white hover:text-primary cursor-pointer"
+        @click="isShown = !isShown"
+      />
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    closable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isShown: true,
+    };
+  },
+});
+</script>
